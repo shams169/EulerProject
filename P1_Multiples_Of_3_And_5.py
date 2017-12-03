5f9415891bba7042763d7158ef259858e93a88aa
shams <shamssss.169@gmail.com>
2017-12-03 15:08:07 -0700

    Problem 1

---
+++ P1_Multiples_Of_3_And_5.py
@@ -0,0 +1,14 @@
+def multiples_test(n):
+    sum = 0
+    for i in range(n):
+        if i%3 == 0 or i%5 == 0:
+            sum += i
+    return sum
+
+
+def main():
+    print(multiples_test(1000))
+
+
+if __name__ == '__main__':
+    main()
